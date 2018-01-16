bcbded6d43acbe976a31558b4ee9010606b305cc
diogofgr <diogofgr@gmail.com>
2018-01-16 12:01:39 +0000

    add map to each table row

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData){
@@ -9,13 +10,14 @@ class WeatherList extends Component {
     const temps = cityData.list.map(item => item.main.temp);
     const pressures = cityData.list.map(item => item.main.pressure);
     const humidities = cityData.list.map(item => item.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="red" /></td>
-        <td><Chart data={pressures} color="green" /></td>
-        <td><Chart data={humidities} color="blue" /></td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="red" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
 
       </tr>
     );
@@ -27,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperat</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
